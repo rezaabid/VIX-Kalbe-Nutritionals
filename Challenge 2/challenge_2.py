2d14609e601a5aff927a12b48b7d4c7bfd151496
Reza Abid Abistha <reza.abid.abistha-2019@fst.unair.ac.id>
2023-07-28 17:54:39 +0700

    Update challenge 2 file

--- Challenge 2/challenge_2.py
+++ Challenge 2/challenge_2.py
@@ -1,5 +1,7 @@
+# Import mysql connector
 import mysql.connector
 
+# Do a conenction test
 conn = mysql.connector.connect(
     host='localhost',
     user='root',
@@ -7,16 +9,22 @@ conn = mysql.connector.connect(
     database='karyawan'
 )
 
+# Create a cursor object as a marker
 cursor = conn.cursor()
 
+# Declare SQL Query to insert record into 'karyawan' database
 insert_sql = 'INSERT INTO biodata (first_name, last_name, age, sex, income) VALUES (%s, %s, %s, %s, %s)'
 values = ('Jason', 'Moreno', 23, 'Male', 550)
 
 try:
+    # Execute SQL command
     cursor.execute(insert_sql, values)
+    # Make changes (commit) to database
     conn.commit()
 except:
+    # DO a rollback if an error occurs or there is an issue
     conn.rollback()
 
+# Close the connection
 cursor.close()
 conn.close()
